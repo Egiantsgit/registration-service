bf46c3de7d05bbcb60f6a856207e5b57cd97ef51
egiants.git <egiants.git@gmail.com>
2018-06-16 02:53:17 -0400

    Controller Tests

--- registration-service/src/main/java/com/egiants/rlm/controller/UserController.java
+++ registration-service/src/main/java/com/egiants/rlm/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.egiants.rlm.controller;
 
-import java.util.List;
-
+import com.egiants.rlm.Exceptions.MismatchIdentifierException;
+import com.egiants.rlm.entity.User;
+import com.egiants.rlm.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,69 +15,66 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.egiants.rlm.entity.User;
-import com.egiants.rlm.service.UserService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 @Api(value = "Registration", description = "Operations pertaining to users") // swagger annotation
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	// swagger annotation
-	@ApiOperation(value = "view list of users")
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> getUsers() {
-		return new ResponseEntity<>(this.userService.getUsers(), HttpStatus.OK);
-	}
+    @ApiOperation(value = "List of Users")
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(this.userService.getUsers(), HttpStatus.OK);
+    }
 
-	// swagger annotation
-	@ApiOperation(value = "search a user with email")
-	@RequestMapping(value = "/{emailId:.+}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE })
-	public ResponseEntity<User> getUser(@PathVariable("emailId") String emailId) {
+    @ApiOperation(value = "Get User with emailId")
+    @RequestMapping(value = "/{emailId:.+}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<User> getUser(@PathVariable("emailId") String emailId) {
 
-		return new ResponseEntity<>(this.userService.getUser(emailId), HttpStatus.OK);
-	}
+        return new ResponseEntity<>(this.userService.getUser(emailId), HttpStatus.OK);
+    }
 
-	// swagger annotation
-	@ApiOperation(value = "Add a user")
-	@RequestMapping(value = "/{emailId:.+}", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE })
-	public ResponseEntity<User> createUser(@PathVariable("emailId") String emailId, @RequestBody User user) {
+    @ApiOperation(value = "Add User")
+    @RequestMapping(value = "/{emailId:.+}", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<User> createUser(
+            @PathVariable("emailId") String emailId, @RequestBody User user) {
 
-		// TODO: add MISMATCH identifier validation exception
+        if (!emailId.equals(user.getEmailId())) {
+            throw new MismatchIdentifierException(emailId);
+        }
 
-		return new ResponseEntity<>(this.userService.createUser(user), HttpStatus.CREATED);
-	}
+        return new ResponseEntity<>(this.userService.createUser(user), HttpStatus.CREATED);
+    }
 
-	// swagger annotation
-	@ApiOperation(value = "update a user")
-	@RequestMapping(value = "/{emailId:.+}", method = RequestMethod.PUT, consumes = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE })
-	public ResponseEntity<User> updateUser(@PathVariable("emailId") String emailId, @RequestBody User user) {
+    @ApiOperation(value = "Update User")
+    @RequestMapping(value = "/{emailId:.+}", method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<User> updateUser(
+            @PathVariable("emailId") String emailId, @RequestBody User user) {
 
-		// TODO: add MISMATCH identifier validation exception
+        if (!emailId.equals(user.getEmailId())) {
+            throw new MismatchIdentifierException(emailId);
+        }
 
-		return new ResponseEntity<>(this.userService.updateUser(user), HttpStatus.CREATED);
-	}
+        return new ResponseEntity<>(this.userService.updateUser(user), HttpStatus.OK);
+    }
 
-	// swagger annotation
-	@ApiOperation(value = "delete a user")
-	@RequestMapping(value = "/{emailId:.+}", method = RequestMethod.DELETE, produces = {
-			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @ApiOperation(value = "Delete User")
+    @RequestMapping(value = "/{emailId:.+}", method = RequestMethod.DELETE, produces = {
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 
-	public ResponseEntity<Void> deleteUser(@PathVariable("emailId") String emailId) {
+    public ResponseEntity<Void> deleteUser(@PathVariable("emailId") String emailId) {
 
-		this.userService.deleteUser(emailId);
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-	}
+        this.userService.deleteUser(emailId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
--- registration-service/src/test/java/com/egiants/rlm/service/UserServiceImplTest.java
+++ registration-service/src/test/java/com/egiants/rlm/service/UserServiceImplTest.java
@@ -1,11 +1,7 @@
 package com.egiants.rlm.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-
-import java.util.Arrays;
-import java.util.List;
-
+import com.egiants.rlm.dao.UserDao;
+import com.egiants.rlm.entity.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -13,85 +9,88 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import com.egiants.rlm.dao.UserDao;
-import com.egiants.rlm.entity.User;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
 public class UserServiceImplTest {
-	
-	private static final String EMAIL_ID = "xyz@gmail";
-	private static final String FIRST_NAME = "firstName";
-	
-	private User user;
-	private List<User> users;
-	
-	@Mock
-	private UserDao userDao;
-	
-	@InjectMocks
-	private UserServiceImpl userService;
 
-	@Before
-	public void setUp() throws Exception {
-		
-		this.user = Mockito.mock(User.class);
-		this.users = Arrays.asList(user);
-		
-		Mockito.doReturn(FIRST_NAME)
-		.when(user)
-		.getFirstName();
-		
-		MockitoAnnotations.initMocks(this);
-		
-	}
+    private static final String EMAIL_ID = "xyz@gmail";
+    private static final String FIRST_NAME = "firstName";
 
-	@Test
-	public void testGetUsers() {
-		Mockito.doReturn(this.users)
-		.when(this.userDao)
-		.getUsers();
-		
-		List<User> users = this.userService.getUsers();
-		assertEquals(FIRST_NAME, users.get(0).getFirstName());
-	}
+    private User user;
+    private List<User> users;
 
-	@Test
-	public void testGetUser() {
-	Mockito.doReturn(user)
-	.when(this.userDao)
-	.getUser(EMAIL_ID);
-	
-	User actualUser = this.userService.getUser(EMAIL_ID);
-	assertEquals(FIRST_NAME, actualUser.getFirstName());
-	}
+    @Mock
+    private UserDao userDao;
 
-	@Test
-	public void testCreateUser() {
-		Mockito.doReturn(this.user)
-		.when(this.userDao)
-		.createUser(this.user);
-		
-		User actualUser = this.userService.createUser(user);
-		assertEquals(FIRST_NAME, actualUser.getFirstName());
-	}
+    @InjectMocks
+    private UserServiceImpl userService;
 
-	@Test
-	public void testCreateOrUpdateUser() {
-		Mockito.doReturn(this.user)
-		.when(this.userDao)
-		.createOrUpdateUser(this.user);
-		
-		User actualUser = this.userService.createOrUpdateUser(user);
-		assertEquals(FIRST_NAME, actualUser.getFirstName());
-	}
+    @Before
+    public void setUp() throws Exception {
 
-	@Test
-	public void testDeleteUser() {
-		Mockito.doNothing()
-		.when(this.userDao)
-		.deleteUser(EMAIL_ID);
-		
-		this.userService.deleteUser(EMAIL_ID);
-		verify(userDao).deleteUser(EMAIL_ID);
-	}
+        this.user = Mockito.mock(User.class);
+        this.users = Arrays.asList(user);
+
+        Mockito.doReturn(FIRST_NAME)
+                .when(user)
+                .getFirstName();
+
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    public void testGetUsers() {
+        Mockito.doReturn(this.users)
+                .when(this.userDao)
+                .getUsers();
+
+        List<User> users = this.userService.getUsers();
+        assertEquals(FIRST_NAME, users.get(0).getFirstName());
+    }
+
+    @Test
+    public void testGetUser() {
+        Mockito.doReturn(user)
+                .when(this.userDao)
+                .getUser(EMAIL_ID);
+
+        User actualUser = this.userService.getUser(EMAIL_ID);
+        assertEquals(FIRST_NAME, actualUser.getFirstName());
+    }
+
+    @Test
+    public void testCreateUser() {
+        Mockito.doReturn(this.user)
+                .when(this.userDao)
+                .createUser(this.user);
+
+        User actualUser = this.userService.createUser(user);
+        assertEquals(FIRST_NAME, actualUser.getFirstName());
+    }
+
+    @Test
+    public void testCreateOrUpdateUser() {
+        Mockito.doReturn(this.user)
+                .when(this.userDao)
+                .updateUser(this.user);
+
+        User actualUser = this.userService.updateUser(user);
+        assertEquals(FIRST_NAME, actualUser.getFirstName());
+    }
+
+    @Test
+    public void testDeleteUser() {
+        Mockito.doNothing()
+                .when(this.userDao)
+                .deleteUser(EMAIL_ID);
+
+        this.userService.deleteUser(EMAIL_ID);
+        verify(userDao).deleteUser(EMAIL_ID);
+    }
 
 }
